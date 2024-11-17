9b5eccb4fbd0e2e978fe5516d7c9f87e58adabc7
rahul0842singh <42551992+rahul0842singh@users.noreply.github.com>
2024-11-17 21:22:42 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -196,7 +196,7 @@ const Home = () => {
                         <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="600">
                             <div class="box">
                                 <img src={values2} class="img-fluid" alt="" />
-                                <h3>Protecting Your Information, Ensuring Privacy.</h3>
+                                <h3>Protecting our Information, Ensuring Privacy.</h3>
                                 <p>We understand the importance of keeping your data secure and system employs advanced security .</p>
                             </div>
                         </div>
